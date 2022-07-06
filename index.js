9f33ea66b63eedc748f3af00473d039e1020a600
jhwoo <jhwoo@withnetworks.com>
2022-07-06 14:26:05 +0900

    yaml파일 분리

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
+const swaggerDocument = YAML.load(path.join(__dirname,'./swagger/output.yaml'))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
